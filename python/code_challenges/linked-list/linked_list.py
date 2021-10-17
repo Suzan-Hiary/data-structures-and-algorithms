fdb8ebdcbe9926ecd0950bd07bd5fff6fb082aa4
Suzan-Hiary <suze-hiary@hotmail.com>
2021-10-17 23:41:34 +0300

    code-6

--- python/code_challenges/linked-list/linked_list.py
+++ python/code_challenges/linked-list/linked_list.py
@@ -1,127 +1,143 @@
+
 # Defines a node in the singly linked list
+
 class Node:
+ 
+  def __init__(self, data, next_ = None):
+    self.data = data
+    self.next = next_
 
-    def __init__(self, value, next_node = None):
-        self.value = value
-        self.next = next_node
-
-# Defines the singly linked list
+    # Defines the singly linked list
 class LinkedList:
-    def __init__(self):
-      self.head = None # keep the head private. Not accessible outside this class
+ 
 
-    # returns the value in the first node
-    # returns None if the list is empty
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def get_first(self):
-        pass
+  def __init__(self):
+    self.head = None
+
+  def insert(self, value):
+
+    '''
+    takes in a value and insert this value with a refernce to the old head node abd push this value 
+    to be the new head of linked list 
+    '''   
+    # create new node
+    self.head = Node(value, self.head)
+    return self.head 
+
+  # def includes(self,value):
+      
+  def includes(self,value):
+      '''
+    returns a bollean if the nodes exiest in the linked list
+     and false if not 
+    '''
+         
+  #   Current <-- Head
+      current=self.head
+  # WHILE Current is not NULL
+      while current != None:
+  #   IF Current.Value is equal to value
+        if current.data==value:
+  #     return TRUE
+           return True
+  #   Current <-- Current.Next
+        else:
+          current=current.next
+      
+      return False
+
+  def __str__(self):
+   '''
+  to string
+Arguments: none
+Returns: a string representing all the values in the Linked List,
+ formatted as:
+"{ a } -> { b } -> { c } -> NULL"
+  '''
+   string = ""
+   current=self.head
+  # WHILE Current is not NULL
+   while current != None:
+  #   IF Current.Value is equal to value
+       value = current.data
+       if current.next is None:
+          string +="{"+f' {value} '+"}" + " -> NULL"
+          break
+
+       else:
+          string +="{"+f' {value} '+"} -> "
+          current = current.next
+    #   Current <-- Current.Next
+  #     return TRUE
+   return string
+   
+  def append(self,value):
+      '''
+        this function will look for the nide that has node.next as none which
+        will represnt the the last node then it will append the value(node)
+        and it will give the new created node to that node .next
+      '''
+      current=self.head
+      while current :
+  #   IF Current.next is equal to none that means this is the last node 
+        if current.next==None:
+          
+          current.next=Node(value)
+          break
+        current=current.next           
+  #   Current <-- Current.Next
+
+            
+      
+  def append_multi(self,list):
+    '''
+    takes in s list of nodes and add them to the end of the 
+    linked list 
+    '''
+    for item in list:
+        self.append(item)
+    return ll 
 
 
-    # method to add a new node with the specific data value in the linked list
-    # insert the new node at the beginning of the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def add_first(self, value):
-        pass
+  def insertBefore(self, targetValue, value):
+    # create new node
+    newNode = Node(value)
+    # find target node to insert
+    node = self.head
+    if node != None:
+        # search nodes
+        while node:
+            if node.next == None:
+                break
+            if node.next.data == targetValue:
+                newNode.next = node.next
+                node.next = newNode
+                break
+            else:
+                node = node.next
+  def insert_after(self,value,new_value):
+    current = self.head
+    while current is not None:
+        if current.data == value:
+            break
+    current = current.next
+    if current is None:
+        print("item not in the list")
+    else:
+        new_node = Node(new_value)
+        new_node.next = current.next
+        current.next = new_node
 
-    # method to find if the linked list contains a node with specified value
-    # returns true if found, false otherwise
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def search(self, value):
-        pass
-
-    # method that returns the length of the singly linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def length(self):
-        pass
-
-    # method that returns the value at a given index in the linked list
-    # index count starts at 0
-    # returns None if there are fewer nodes in the linked list than the index value
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def get_at_index(self, index):
-        pass
-
-    # method that returns the value of the last node in the linked list
-    # returns None if the linked list is empty
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def get_last(self):
-        pass
-
-    # method that inserts a given value as a new last node in the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def add_last(self, value):
-        pass
-
-    # method to return the max value in the linked list
-    # returns the data value and not the node
-    def find_max(self):
-        pass
-
-    # method to delete the first node found with specified value
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def delete(self, value):
-        pass
-
-    # method to print all the values in the linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def visit(self):
-        helper_list = []
-        current = self.head
-
-        while current:
-            helper_list.append(str(current.value))
-            current = current.next
-        
-        print(", ".join(helper_list))
-
-    # method to reverse the singly linked list
-    # note: the nodes should be moved and not just the values in the nodes
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def reverse(self):
-        pass
-  
-    ## Advanced/ Exercises
-    # returns the value at the middle element in the singly linked list
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def find_middle_value(self):
-        pass
-
-    # find the nth node from the end and return its value
-    # assume indexing starts at 0 while counting to n
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def find_nth_from_end(self, n):
-        pass
-
-    # checks if the linked list has a cycle. A cycle exists if any node in the
-    # linked list links to a node already visited.
-    # returns true if a cycle is found, false otherwise.
-    # Time Complexity: ?
-    # Space Complexity: ?
-    def has_cycle(self):
-        pass
-
-    # Helper method for tests
-    # Creates a cycle in the linked list for testing purposes
-    # Assumes the linked list has at least one node
-    def create_cycle(self):
-        if self.head == None:
-            return
-
-        # navigate to last node
-        current = self.head
-        while current.next != None:
-            current = current.next
-
-        current.next = self.head # make the last node link to first node
+ll=LinkedList()
+ll.insert(1)
+ll.insert(2) 
+ll.insert(3) 
+ 
+ll.insert(5)
+ll.insert(4)
+print(ll)
+# ll.append_multi([5,3,3])
+# print(ll.insertBefore(1,3))
+# ll.insert_after(1,4)
+# print(ll)
+# print(str(ll)) 
