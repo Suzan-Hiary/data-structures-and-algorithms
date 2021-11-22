1eb24f225eac4aa9aadbf3c7da4b8386f85c33dc
Suzan-Hiary <suze-hiary@hotmail.com>
2021-11-22 23:13:42 +0200

    code31

--- python/code_challenges/hashmap/hashmap.py
+++ python/code_challenges/hashmap/hashmap.py
@@ -1,26 +1,63 @@
+class Node:
+    def __init__(self, value=None, next_=None):
+        """
+      create a new node
+      """
+        self.value = value
+        self.next = next_
 
-import re
+
+class LinkedList:
+    def __init__(self):
+        """
+        The constructor method for the linked list. Initializes the head of a linked list to None.
+        """
+        self.head = None
+
+    def insert(self, value):
+        """
+        Take a value and store it in a Node, then insert it to the beginning of the linked list.
+        """
+        self.head = Node(value, self.head)
+
+
+class HashTable:
+    def __init__(self, size=1024):
+        """
+        Initalization of Hash table
+        
+        """
+        self.__size = size
+        self.__buckets = [None] * size
+
+    def __hash(self, key):
+        """
+        Takes a key which is a string and returns an integer which is the index that will be used to store the key/value pari in a Node at that index.
+        """
+        return sum([ord(char) for char in key]) * 7 % self.__size
+
+    def add(self, key, value):
+        """
+        A method for adding a new value to the map
+        Arg: Takes the key and value 
+        Return : No return value
+        """
+
+        index = self.__hash(key)
+
+        if not self.__buckets[index]:
+          self.__buckets[index] = LinkedList()
+        my_value = [key,value]
+        self.__buckets[index].insert(my_value)
+        return index
 
 def repeated_word(string):
-    """
-    method that takes a string as a parameter ,then returns the first repeated word in the string 
-    """
-    string = re.sub(r'[^\w\s]', '', string)
-    all_the_words = {}
-    first_repeated_word = ''
-
-    
-    for word in string.split():
-        word = word.lower()
-        try:
-            all_the_words[word] += 1
-            first_repeated_word = first_repeated_word or word
-        except:
-            all_the_words[word] = 1
-
-    
-
-    # check for  matching words
-    if first_repeated_word:
-        return [first_repeated_word]
-    return ["No repeating words found"]
+    arr_of_index=[]
+    hash_map=HashTable()
+    list=string.split(' ')
+    for word in list:
+        word=word.lower().strip(',')
+        index = hash_map.add(word,word)
+        if index in arr_of_index:
+            return word
+        arr_of_index.append(index)
--- test.py
+++ test.py
@@ -1,103 +1,156 @@
-# from typing import Counter
 
 
-# class Node :
-#     """ method to create a new Node"""
 
-#     def __init__(self , data , next = None):
-#         self.data = data
-#         self.next = next 
+class Node :
+    """ method to create a new Node"""
 
-
-# class LinkedList:
-
-#     def __init__(self):
-#         self.head =None
+    def __init__(self , data , next = None):
+        self.data = data
+        self.next = next 
 
 
-#     def insert(self , value):
-#         self.head = Node(value,self.head)
-#         return self.head
+class LinkedList:
+
+    def __init__(self):
+        self.head =None
+
+
+    def insert(self , value):
+        self.head = Node(value,self.head)
+        return self.head
     
-#     def append(self , value):
-#         currentnode = self.head
-#         while currentnode != None:
-#             if currentnode.next is None :
-#                   currentnode.next = Node(value)
-#                   break
-#             currentnode = currentnode.next
+    def append(self , value):
+        currentnode = self.head
+        while currentnode != None:
+            if currentnode.next is None :
+                  currentnode.next = Node(value)
+                  break
+            currentnode = currentnode.next
     
-#     def __str__(self):
+    def __str__(self):
         
-#         string = ""
-#         currentnode=self.head
+        string = ""
+        currentnode=self.head
 
-#         while currentnode != None:
-#             value = currentnode.data
-#             if currentnode.next is None:
-#                 string += "{" + f'{value}' + '}' + "-> Null"
-#                 break
+        while currentnode != None:
+            value = currentnode.data
+            if currentnode.next is None:
+                string += "{" + f'{value}' + '}' + "-> Null"
+                break
                 
-#             else:
-#                 string += "{" + f'{value}' + '}' + "-> "
-#                 currentnode = currentnode.next
-#         return string       
+            else:
+                string += "{" + f'{value}' + '}' + "-> "
+                currentnode = currentnode.next
+        return string       
         
                    
-#     def multi_append(self , list):
-#         """method takes an array as a args and append the list to the end of the list"""
+    def multi_append(self , list):
+        """method takes an array as a args and append the list to the end of the list"""
 
-#         for i in list:
-#             self.append(i)
-#         return
+        for i in list:
+            self.append(i)
+        return
     
-#     def insertbefore(self , target , value):
-#         """ method takes the value and the target value and return the value inserted before the target value"""
+    def insertbefore(self , target , value):
+        """ method takes the value and the target value and return the value inserted before the target value"""
 
-#         newnode = Node(value)
-#         current = self.head
+        newnode = Node(value)
+        current = self.head
         
-#         if current != None:
-#             while current:
+        if current != None:
+            while current:
                 
-#                 if current.next.data == target:
-#                     newnode.next = current.next
-#                     current.next = newnode
-#                     break
-#                 else:
-#                     current = current.next
+                if current.next.data == target:
+                    newnode.next = current.next
+                    current.next = newnode
+                    break
+                else:
+                    current = current.next
                     
 
-#     def insertafter(self,target ,value):
+    def insertafter(self,target ,value):
 
-#         newnode = Node(value)
-#         currentnode=self.head
+        newnode = Node(value)
+        currentnode=self.head
 
-#         if currentnode != None:
-#             while currentnode:
-#                 if currentnode.data == target :
-#                     newnode.next = currentnode.next
-#                     currentnode.next = newnode
-#                     break
-#                 else:
-#                     currentnode = currentnode.next
+        if currentnode != None:
+            while currentnode:
+                if currentnode.data == target :
+                    newnode.next = currentnode.next
+                    currentnode.next = newnode
+                    break
+                else:
+                    currentnode = currentnode.next
+    
+    def reverse(self , list):
+        """ method to reverse the linked list"""
+
+        prev = None
+        current = list.head
+        while(current is not None):
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        list.head = prev
+        return list
+
+    def length(self):
+        """ method to calculate the linked list length"""
+        current = self.head
+        count=0
+
+        while current :
+            count+=1
+            current=current.next
+        return count    
 
 
-#     def zip(list1 , list2):
+    def KTH_from_end(self , k):
+        """ a method takes a number as a parameter and return the value which k is places from the tail"""
+
+         # if k was greater than the linkedlist length
+
+        if k > self.length():
+            return ' The number is greater than the list length'
+        if k < 0 :
+            k = abs(k)-1
+            list = self
+        else :
+            #reverse the array
+            list = LinkedList.reverse(self)
+        
+        counter = 0
+        current = self.head
+
+        while current :
+            if k == current :
+                data = current.data
+                break
+
+        
+            counter+=1
+            current= current.next
+        print(str(self))
+        return data
+
+
 
      
 
 
 
 
-# ll=LinkedList()
-# ll.insert(1)
-# ll.insert(2) 
-# ll.insert(3) 
+ll=LinkedList()
+ll.insert(1)
+ll.insert(2) 
+ll.insert(3) 
 
-# ll.insert(5)
-# ll.append(4)
+ll.insert(5)
+ll.append(4)
 # print(ll.insertbefore(1,99))
 # print(ll.insertafter(1,99))
 # # ll.multi_append([4,5,6,7])
-# print(ll)
+# ll.KTH_from_end(3)
+print(ll.KTH_from_end(3))
+print(ll)
